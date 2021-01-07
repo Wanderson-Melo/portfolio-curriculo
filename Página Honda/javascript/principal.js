c87648cc4a2b253c62f1e7158aafda8591506f7e
Wanderson-Melo <75035633+Wanderson-Melo@users.noreply.github.com>
2021-01-07 19:29:19 -0300

    Subindo site Honda

--- Página Honda/javascript/principal.js
+++ Página Honda/javascript/principal.js
@@ -2,31 +2,58 @@
 let testMenuBurger = document.getElementById('check-menu');
 let textMenuBurger = document.querySelector('.menu label h3');
 
-testMenuBurger.onclick = function(){
+function abreMenu(){
     if(testMenuBurger.value === 'marcado'){
         textMenuBurger.textContent = 'FECHAR';
         testMenuBurger.value = 'desmarcado';
+        if(subLista.innerHTML !== ''){
+            dropList();
+        }
     } else {
         textMenuBurger.textContent = 'MENU';
         testMenuBurger.value = 'marcado';
     }
 }
 
+let trocarSetaLista = document.querySelector('.seta img');
+//alert(trocarSetaLista.src)
+
+function trocaCorSeta(){
+    trocarSetaLista.src = 'imagens/seta-3d3d3d.png';
+}
+let abreMenu2 = document.querySelector('#check-menu');
+function trocaCorSeta2(){
+    if(subLista.innerHTML !== ''){
+        dropList();
+    }
+    if(testMenuBurger.value === 'desmarcado'){        
+        abreMenu2.checked = false;
+        abreMenu();
+    }
+    trocarSetaLista.src = 'imagens/seta-ffffff.png';  
+}
+
 let ativaSubLista = document.getElementById('lista');
 let subLista = document.querySelector('.lista ul');
 
-let trocarSetaLista = document.querySelector('.seta img');
-trocarSetaLista.setAttribute('id', 'foto');
+
 //let corpoMain = document.getElementById('main'); //ainda não está funcionando, pretendo mudar a cor do background
 
 let textProdutos = document.querySelector('.lista h3')
 
-ativaSubLista.onclick = function(){
+//ativaSubLista.onclick = dropList();
+
+function dropList(){
     if(subLista.innerHTML === ''){
         trocarSetaLista.setAttribute('src', 'imagens/seta-cc0000.png');
         trocarSetaLista.style.transform = "rotateX(180deg)"
         trocarSetaLista.style.padding = '0px 0px 15px 0px';
         textProdutos.style.color = '#cc0000'
+
+        if(testMenuBurger.value === 'desmarcado'){        
+            abreMenu2.checked = false;
+            abreMenu();
+        }
 
         let itemSubLista = document.createElement('li');
         itemSubLista.innerHTML = `Motores`;
@@ -55,4 +82,4 @@ ativaSubLista.onclick = function(){
 }
 trocarSetaLista.style.transition = '.5s';
 itemSubLista.style.transition = '1s';
-
+document.body.style.transition = 'all 1s'
